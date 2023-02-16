fad44cceaf0a4f19d4a71966c4e1fbc08089b4c4
susanna <sus@gmail.com>
2023-02-16 23:05:53 +0300

    задача13

---
+++ zadacha10/zadacha13/Zadacha13.cs
@@ -0,0 +1,27 @@
+﻿int number = ReadInt("Введите число: ");
+int count = number.ToString().Length;
+Console.Write(MakeArray(number, count));
+int ReadInt(string message)
+{
+ Console.Write(message);
+ return Convert.ToInt32(Console.ReadLine());
+}
+int MakeArray(int a, int b)
+{
+int result = 0;
+ if (b < 3)
+    {
+ Console.Write("Третьей цифры нет:");
+    }
+ else
+    {
+ int c = 1;
+ for (int i = b; i > 3; i--)
+        {
+ c = c * 10;
+        }
+
+ result = (a / c) % 10;
+    }
+return result;
+}
